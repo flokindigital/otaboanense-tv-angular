78d0974164e479d423f04dfddb9a9dca73ac6f0d
Sérgio Cardoso <contato@flok.in>
2019-04-11 17:28:24 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SPA-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'SPA';
+}
